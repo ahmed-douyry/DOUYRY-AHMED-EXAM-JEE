4862ee3d320d4fc6fce3375aa75c3ba9fb78d46c
ahmed-douyry <ahmed.douyry@gmail.com>
2026-05-11 14:25:05 +0100

    initialisation de projet

---
+++ backend/src/test/java/com/example/douyry_ahmed/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.douyry_ahmed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
